78b74cbf9387778ba9694a1e55d8c122074caf34
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-07-26 18:45:01 +0545

    Home Page front End completed

--- home.php
+++ home.php
@@ -52,17 +52,97 @@ $result = mysqli_fetch_assoc($data);
             
     </div>
 
+    <div class="profile-section">
+        <div class="profile-picture">
+            <img src="images/logo.png" alt="">
+        </div>
+        <span id="account-name">Gandaki Medical College</span>
+        <div class="profile-info">
+            <br><br><br>
+             <i class="fas fa-mobile-alt"></i> 0565-70053 <br> <br>
+             <i class="fas fa-map-marker-alt"></i> State 4 - Kaski - Pokhara <br> <br>
+             <i class="fas fa-envelope"></i> <?php echo $result['EMAIL'] ; ?> <br>
+                <hr>
+          
+             Has someone donated blood ? Don't forget to register them here.
+             <br> <br>
+             <button >Keep Record</button>
+        </div>
+    </div>
+
+    <div class="task-to-do">
+        <div class="get-donor-list">
+            <br><br>
+            <form action="">
+            <select name="" id="" class="round" required>
+                       <option value="" disabled selected>Select Blood Group</option>
+                       <option value="">A+</option>
+                       <option value="">A-</option>
+                       <option value="">B+</option>
+                       <option value="">B-</option>
+                       <option value="">O+</option>
+                       <option value="">O-</option>
+                       <option value="">AB+</option>
+                       <option value="">AB-</option>
+            </select>
+            <br><br>
+
+            <input type="submit" value = "Get the Donor List">
+
+
+            </form>
+            
+    
+
+        </div>
+
+        <div class="post-blood-request">
+        <br><br>
+        <form action="">
+            <select name="" id="" class="round" required>
+                       <option value="" disabled selected>Select Blood Group</option>
+                       <option value="">A+</option>
+                       <option value="">A-</option>
+                       <option value="">B+</option>
+                       <option value="">B-</option>
+                       <option value="">O+</option>
+                       <option value="">O-</option>
+                       <option value="">AB+</option>
+                       <option value="">AB-</option>
+            </select>
+            <br><br>
+
+            <input type="submit" value = "Post Blood Request">
+
+
+            </form>
+
+        </div>
+
+    </div>
+
+    <!-- Footer -->
+    <div class="footer">
+            | <a href="about.html" target="_Blank">About</a>
+            | <a href="privacy.html" target="_Blank">Privacy Policy</a>
+            | <a href="terms.html" target="_Blank">Terms</a>
+            | <a href="cookies.html" target="_Blank">Cookies Policy</a>
+            | <a href="help.html" target="_Blank">Help</a>
+            | ShareBlood &copy; 2019 |
+        </div>
+
 
     <script src="js/home.js"></script>
     
 </body>
 </html>
 
+
 <?php
 
-echo "Welcome" . "<br>" ;
-echo "ID : ".$result['ID']."<br>"; 
-echo "Email : ".$result['EMAIL']."<br>";
-echo "Password : " . $result['PASSWORD']."<br>";
+// echo "Welcome" . "<br>" ;
+// echo "ID : ".$result['ID']."<br>"; 
+// echo "Email : ".$result['EMAIL']."<br>";
+// echo "Password : " . $result['PASSWORD']."<br>";
 
 ?>

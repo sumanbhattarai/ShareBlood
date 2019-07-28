dfd7af91d67e798d1373d6929f9ea21940285497
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-07-28 14:38:47 +0545

    Updated

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
 
+<!-- establish connection -->
+<?php
+    session_start();
+    include("connection.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,14 +21,38 @@
     </div>
 
     <!-- body -->
+    <p class="error-message">
+    <?php
+
+        if(isset($_POST['login']))
+        {
+            $email = $_POST['email'] ;
+            $password = $_POST['password'] ;
+            $query = " SELECT * FROM try WHERE EMAIL='$email' && PASSWORD=='$password' " ;
+            $data = mysqli_query($conn , $query) ;
+            $total = mysqli_num_rows($data) ;
+            if($total !=0)
+            {
+                $_SESSION['email'] = $email ; 
+                header('location:home.php');
+            }
+            else
+            {
+                echo "* Invalid Email or Password" ;
+            }
+        }
+
+
+    ?>
+    </p>
     <div class = "login-box">
         <div class ="inner-box">
             <br>
             <span id="login-text">Log into ShareBlood</span> <br> <br>
-            <form action="">
+            <form action="" method = "POST">
                 <input type="email" name="email" placeholder="Email" required> <br> <br>
                 <input type="password" name="password" placeholder="Password" required> <br><br>
-                <input type="submit" value = "login" name="submit">
+                <input type="submit" value = "Login" name="login">
                 <br><br>
                 <span id="forget-password"><a href="#">Forget Password ? </a></span>
                  

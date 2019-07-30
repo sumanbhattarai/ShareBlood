f45391d91ca7215a4395e44759031a4fce11b218
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-07-30 19:40:34 +0545

    Redirect to home.php if session has been set

--- login.php
+++ login.php
@@ -3,6 +3,10 @@
 <?php
     session_start();
     include("connection.php");
+    if(isset($_SESSION['email']))
+    {
+    header('location:home.php') ;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">

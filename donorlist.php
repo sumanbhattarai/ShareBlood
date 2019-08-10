d680419f93c370a308d4d697ba11ceb6203d6de7
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-08-10 17:07:14 +0545

    Preveting access through url

--- donorlist.php
+++ donorlist.php
@@ -2,6 +2,18 @@
 error_reporting(0);
 include('modules/connection.php');
 include('modules/session.php') ;
+            /* preventing file from direct access */
+            if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+                /* 
+                   Up to you which header to send, some prefer 404 even if 
+                   the files does exist for security
+                */
+                header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+        
+                /* choose the appropriate page to redirect users */
+                die( header( 'location: home.php' ) );
+        
+            }
 
 ?>
 <!DOCTYPE html>

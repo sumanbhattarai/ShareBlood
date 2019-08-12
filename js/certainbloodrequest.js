49258d42fd38bc13c56f2a73e3ff65d35b500ecb
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-08-12 22:22:09 +0545

    Filter feature added

---
+++ js/certainbloodrequest.js
@@ -0,0 +1,36 @@
+const shareBlood = document.getElementById('shareblood') ;
+
+// Adding mouseover event
+
+shareBlood.addEventListener('mouseover' , function(){
+    // console.log("Mouse is over");
+    shareBlood.style.cursor = 'pointer';
+});
+
+shareBlood.addEventListener('mouseout' , function(){
+    // console.log("Mouse is out");
+});
+
+shareBlood.addEventListener('click' , function(){
+    window.location.href='index.php' ;
+});
+
+
+// whenever page is scrolled  it involed scrollFunction.
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("goto-top").style.display = "block";
+  } else {
+    document.getElementById("goto-top").style.display = "none";
+  }
+}
+
+//onclicking to goto top button it invokes this function
+function gotoTop()
+{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
